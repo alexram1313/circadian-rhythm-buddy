8cc07fc483d87d2eeb3dfeded4b139e2da37c2b3
Alex Ramirez <alexir13@gmail.com>
2019-02-28 15:02:02 -0800

    Change from EVSE

--- back-end/routes/v1/index.js
+++ back-end/routes/v1/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 
 router.get('/', function(req,res){
-    res.json({'message':"Connected to EVSE API v1"})
+    res.json({'message':"Connected to Circadian Rhythm Buddy API v1"})
 });
 
 module.exports = router;
