036061b5649f5bf29c89d03aff55bb9f52ca6fb5
Alex Ramirez <alexir13@gmail.com>
2019-03-19 03:11:27 -0700

    Topic selection pivot collection

---
+++ back-end/model/user-topic-count.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+const credential = require('../config/credential');
+
+var UserTopicCountSchema = new mongoose.Schema({
+
+    topic_name: {
+        type: String, 
+        required: true,
+    },
+    count: {
+        type: Number,
+        default: 0,
+    }
+})
+mongoose.connect(`mongodb://${credential.MONGO_USER}:${credential.MONGO_PASS}@ds117866.mlab.com:17866/circadian`, {useNewUrlParser: true});
+var UserTopicCount = mongoose.model('UserTopicCount', UserTopicCountSchema);
+
+module.exports = {UserTopicCount};
