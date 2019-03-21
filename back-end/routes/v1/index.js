5f7573278ac05aba679bc511028369a095ed0d7c
Alex Ramirez <alexir13@gmail.com>
2019-03-21 02:37:19 -0700

    dynamic time update

--- back-end/routes/v1/index.js
+++ back-end/routes/v1/index.js
@@ -69,7 +69,7 @@ router.get('/activities', async function(req,res){
             heart_rate: 72
         }
     ];
-    let activities = await recomEngine.search_activities(dataMed, "2019-03-17T15:00:00", "33.6295", "-117.8684" );
+    let activities = await recomEngine.search_activities(dataMed, (new Date()).toISOString(), "33.6295", "-117.8684" );
     console.log(activities);
     res.json(activities);
 });
